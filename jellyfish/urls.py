d2f2396b04f016ae6d8a22a4348d41a2ee73bc23
wingleeio <contact@winglee.io>
2019-01-07 00:11:46 -0800

    hello

--- jellyfish/urls.py
+++ jellyfish/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('forum.urls')),
     path('management/admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
