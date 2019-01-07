b118ed4d6a1a5846a4e8006b46ebffe62454c77c
wingleeio <contact@winglee.io>
2019-01-07 00:06:41 -0800

    hello

--- jellyfish/settings.py
+++ jellyfish/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID = 1
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
