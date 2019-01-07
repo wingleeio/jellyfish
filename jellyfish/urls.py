0ae322ccb16935afae2c01017f4e43b04118640f
wingleeio <contact@winglee.io>
2019-01-07 00:25:07 -0800

    hello

--- jellyfish/urls.py
+++ jellyfish/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('management/admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
