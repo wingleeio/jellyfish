00262965b24b05ba0a8a551f2263ad8847c55c44
wingleeio <contact@winglee.io>
2019-01-07 00:54:39 -0800

    hello

--- jellyfish/settings.py
+++ jellyfish/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -176,3 +177,5 @@ SITE_ID = 1
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
